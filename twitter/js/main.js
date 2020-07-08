76e582b63b46c7810fe1a0fa67a7e72b657002ed
ShimejiOzaki <sdkfz181tiger@gmail.com>
2020-07-08 15:19:46 +0900

    test

--- twitter/js/main.js
+++ twitter/js/main.js
@@ -3,7 +3,12 @@
 $(document).ready(()=>{
 	console.log("Ready");
 	$("#info").text("location.search:" + location.search);
+
+
+
+	/*
 	if(location.search !== ""){
 		location.href="twittersdk://path"+location.search;
 	}
+	*/
 });
