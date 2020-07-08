05cc72126cdf70fe5ad2e3813548884816ff28b7
ShimejiOzaki <sdkfz181tiger@gmail.com>
2020-07-08 13:21:37 +0900

    test

--- twitter/js/main.js
+++ twitter/js/main.js
@@ -4,6 +4,6 @@ window.onload = ()=>{
 	console.log("Callback to application!!");
 	if(location.search !== ""){
 		console.log("location.search:" + location.search);
-		//location.href="twittersdk://path"+location.search;
+		location.href="twittersdk://path"+location.search;
 	}
 };
