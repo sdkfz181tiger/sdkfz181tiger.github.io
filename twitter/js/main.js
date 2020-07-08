2197a1762c715f9ea413c525f9612a0a1bbc7059
ShimejiOzaki <sdkfz181tiger@gmail.com>
2020-07-08 19:33:32 +0900

    test

--- twitter/js/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-$(document).ready(()=>{
-	console.log("Ready");
-	$("#info").text("location.search:" + location.search);
-	if(location.search !== ""){
-		location.href="twittersdk://path"+location.search;
-	}
-});
