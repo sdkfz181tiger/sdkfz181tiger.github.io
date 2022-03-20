03ad026b7ab54ce0da40ab1b24a44bee948e5955
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2022-03-20 18:13:36 +0900

    Update

--- gamestrawberry/debug/tmx/blocks.tsx
+++ gamestrawberry/debug/tmx/blocks.tsx
@@ -1,4 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="blocks" tilewidth="15" tileheight="15" tilecount="8" columns="8">
  <image source="blocks.png" width="120" height="15"/>
+ <tile id="0" type="tile_01"/>
+ <tile id="1" type="tile_02"/>
+ <tile id="2" type="tile_03"/>
+ <tile id="3" type="tile_04"/>
+ <tile id="4" type="tile_05"/>
+ <tile id="5" type="tile_06"/>
+ <tile id="6" type="tile_07"/>
+ <tile id="7" type="tile_08"/>
 </tileset>
