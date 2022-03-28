f7fb842a9e63bdb73adab8b78303503e02bfa333
Shinichi Ozaki <sdkfz181tiger@gmail.com>
2022-03-28 21:27:40 +0900

    Update

--- gamestrawberry/debug/tmx/blocks.tsx
+++ gamestrawberry/debug/tmx/blocks.tsx
@@ -2,19 +2,19 @@
 <tileset version="1.8" tiledversion="1.8.2" name="blocks" tilewidth="15" tileheight="15" tilecount="16" columns="16">
  <image source="blocks.png" width="240" height="15"/>
  <tile id="0" type="1"/>
- <tile id="1" type="2"/>
- <tile id="2" type="3"/>
- <tile id="3" type="4"/>
- <tile id="4" type="5"/>
- <tile id="5" type="6"/>
- <tile id="6" type="7"/>
- <tile id="7" type="8"/>
- <tile id="8" type="9"/>
- <tile id="9" type="10"/>
- <tile id="10" type="11"/>
- <tile id="11" type="12"/>
- <tile id="12" type="13"/>
- <tile id="13" type="14"/>
- <tile id="14" type="15"/>
- <tile id="15" type="16"/>
+ <tile id="1" type="images/blogk_28x28_02.png"/>
+ <tile id="2" type="images/blogk_28x28_03.png"/>
+ <tile id="3" type="images/blogk_28x28_04.png"/>
+ <tile id="4" type="images/blogk_28x28_05.png"/>
+ <tile id="5" type="images/blogk_28x28_06.png"/>
+ <tile id="6" type="images/blogk_28x28_07.png"/>
+ <tile id="7" type="images/blogk_28x28_08.png"/>
+ <tile id="8" type="images/blogk_28x28_09.png"/>
+ <tile id="9" type="images/blogk_28x28_10.png"/>
+ <tile id="10" type="images/blogk_28x28_11.png"/>
+ <tile id="11" type="images/blogk_28x28_12.png"/>
+ <tile id="12" type="images/blogk_28x28_13.png"/>
+ <tile id="13" type="images/blogk_28x28_14.png"/>
+ <tile id="14" type="images/blogk_28x28_15.png"/>
+ <tile id="15" type="images/blogk_28x28_16.png"/>
 </tileset>
