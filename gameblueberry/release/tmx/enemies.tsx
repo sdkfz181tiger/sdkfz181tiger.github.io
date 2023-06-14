ed213f6b25133e4df80b026d90fc991166f86b43
Kajiru <sdkfz181tiger@gmail.com>
2023-06-14 22:45:48 +0900

    Update

---
+++ gameblueberry/release/tmx/enemies.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="enemies" tilewidth="28" tileheight="28" tilecount="18" columns="9">
+ <image source="enemies.png" width="252" height="56"/>
+</tileset>
---
+++ gameblueberry/release/tmx/items.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="items" tilewidth="28" tileheight="28" tilecount="9" columns="9">
+ <image source="items.png" width="252" height="28"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="xx.png"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="i_apple.png"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="i_blueberry.png"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="i_cherry.png"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" value="i_orange.png"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" value="i_strawberry.png"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" value="i_lemon.png"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="name" value="i_peach.png"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="name" value="i_banana.png"/>
+  </properties>
+ </tile>
+</tileset>
