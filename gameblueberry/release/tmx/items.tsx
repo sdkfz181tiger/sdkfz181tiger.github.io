bcffa7bc8ad0d4da56aee27062a66aa8ce21597c
Kajiru <sdkfz181tiger@gmail.com>
2023-06-25 10:22:02 +0900

    Update

--- gameblueberry/release/tmx/items.tsx
+++ gameblueberry/release/tmx/items.tsx
@@ -1,49 +1,39 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="items" tilewidth="28" tileheight="28" tilecount="9" columns="9">
- <image source="items.png" width="252" height="28"/>
+<tileset version="1.10" tiledversion="1.10.1" name="items" tilewidth="28" tileheight="28" tilecount="7" columns="7">
+ <image source="items.png" width="196" height="28"/>
  <tile id="0">
   <properties>
-   <property name="name" value="xx.png"/>
+   <property name="name" value="noname"/>
   </properties>
  </tile>
  <tile id="1">
   <properties>
-   <property name="name" value="i_apple.png"/>
+   <property name="name" value="radish"/>
   </properties>
  </tile>
  <tile id="2">
   <properties>
-   <property name="name" value="i_blueberry.png"/>
+   <property name="name" value="carrot"/>
   </properties>
  </tile>
  <tile id="3">
   <properties>
-   <property name="name" value="i_cherry.png"/>
+   <property name="name" value="tomato"/>
   </properties>
  </tile>
  <tile id="4">
   <properties>
-   <property name="name" value="i_orange.png"/>
+   <property name="name" value="cabagge"/>
   </properties>
  </tile>
  <tile id="5">
   <properties>
-   <property name="name" value="i_strawberry.png"/>
+   <property name="name" value="corn"/>
   </properties>
  </tile>
  <tile id="6">
   <properties>
-   <property name="name" value="i_lemon.png"/>
-  </properties>
- </tile>
- <tile id="7">
-  <properties>
-   <property name="name" value="i_peach.png"/>
-  </properties>
- </tile>
- <tile id="8">
-  <properties>
-   <property name="name" value="i_banana.png"/>
+   <property name="name" value="eggplant"/>
   </properties>
  </tile>
 </tileset>
