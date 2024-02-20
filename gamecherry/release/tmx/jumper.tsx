ec7c3405e387bfa9d7c789c861aec09817c290e5
Kajiru <sdkfz181tiger@gmail.com>
2024-02-20 21:29:04 +0900

    Update

---
+++ gamecherry/release/tmx/jumper.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="jumper" tilewidth="28" tileheight="28" tilecount="5" columns="5">
+ <image source="jumper.png" width="140" height="28"/>
+</tileset>
