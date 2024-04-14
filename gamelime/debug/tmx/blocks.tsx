81991cde4ecc654691a24d0550c7a270799fe49a
Kajiru <sdkfz181tiger@gmail.com>
2024-04-14 10:07:09 +0900

    Update

---
+++ gamelime/debug/tmx/blocks.tsx
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="blocks" tilewidth="28" tileheight="28" tilecount="18" columns="9">
+ <editorsettings>
+  <export target="blocks.json" format="json"/>
+ </editorsettings>
+ <image source="blocks.png" width="252" height="56"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="xx.png"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="block_28x14_01.png"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="block_28x28_02.png"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="block_28x28_03.png"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="name" value="block_28x28_04.png"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="name" value="block_28x28_05.png"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="name" value="block_28x28_06.png"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="name" value="block_28x28_07.png"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="name" value="block_28x28_08.png"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="name" value="block_28x28_08.png"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="name" value="block_28x28_10.png"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="name" value="block_28x28_11.png"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="name" value="block_28x28_12.png"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="name" value="block_28x28_13.png"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="name" value="block_28x28_14.png"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="name" value="block_28x28_15.png"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="name" value="block_28x28_16.png"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="name" value="block_28x28_17.png"/>
+  </properties>
+ </tile>
+</tileset>
