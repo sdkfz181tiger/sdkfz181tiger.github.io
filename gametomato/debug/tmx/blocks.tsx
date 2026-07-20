3043d152e9091e7ec1e67211a96b53febddd50f1
Kajiru <sdkfz181tiger@gmail.com>
2026-07-20 10:32:03 +0900

    Update

--- gametomato/debug/tmx/blocks.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="blocks" tilewidth="16" tileheight="16" tilecount="21" columns="7">
- <image source="blocks.png" width="112" height="52"/>
-</tileset>
---
+++ gametomato/debug/tmx/floors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="floors" tilewidth="16" tileheight="16" tilecount="21" columns="7">
+ <image source="floors.png" width="112" height="52"/>
+</tileset>
