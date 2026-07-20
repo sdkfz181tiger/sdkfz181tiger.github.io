c12a16c80e4ee3fa914e4185def2d261bd4048a3
Kajiru <sdkfz181tiger@gmail.com>
2026-07-20 10:48:28 +0900

    Update

---
+++ gametomato/debug/tmx/items.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="items" tilewidth="16" tileheight="16" tilecount="21" columns="7">
+ <image source="items.png" width="112" height="52"/>
+</tileset>
