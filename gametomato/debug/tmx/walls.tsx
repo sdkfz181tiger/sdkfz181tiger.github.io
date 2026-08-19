2556b18ee76dd73046e654987ecb442f1091fbab
Kajiru <sdkfz181tiger@gmail.com>
2026-08-19 13:13:23 +0900

    Update

--- gametomato/debug/tmx/walls.tsx
+++ gametomato/debug/tmx/walls.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="walls" tilewidth="16" tileheight="16" tilecount="18" columns="6">
- <image source="walls.png" width="96" height="48"/>
+<tileset version="1.10" tiledversion="1.12.2" name="walls" tilewidth="16" tileheight="16" tilecount="36" columns="6">
+ <image source="walls.png" width="96" height="96"/>
 </tileset>
