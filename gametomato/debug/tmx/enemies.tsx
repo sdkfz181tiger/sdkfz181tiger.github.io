d4df50dd761b85668a46c793948d604985814ef4
Kajiru <sdkfz181tiger@gmail.com>
2026-08-20 20:56:46 +0900

    Update

--- gametomato/debug/tmx/enemies.tsx
+++ gametomato/debug/tmx/enemies.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="enemies" tilewidth="16" tileheight="16" tilecount="18" columns="6">
- <image source="enemies.png" width="96" height="48"/>
+<tileset version="1.10" tiledversion="1.12.2" name="enemies" tilewidth="16" tileheight="16" tilecount="24" columns="6">
+ <image source="enemies.png" width="96" height="64"/>
 </tileset>
